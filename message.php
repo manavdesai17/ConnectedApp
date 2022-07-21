156909b4fab3491c8dab39dba1a09e8560a3b996
Avin Lad <avinlad@gmail.com>
2022-07-21 13:37:44 -0400

    fixed errors

--- message.php
+++ message.php
@@ -2,7 +2,7 @@
 
 // Creating each message as an Object, following an OOP approach
 // Might completely remove
-require_once("user.php")
+require_once("user.php");
 class Message {
     public $userName;  // Need to store the Messages according to each user connected to the server. 
     public $message_time;
@@ -12,7 +12,7 @@ class Message {
     function __construct($userName){
         $this->userName = $userName;
         $this->message_time = time(); //to retrieve when a message was sent
-        set_messageid();
+        $this->set_messageid();
 
     }
 
@@ -22,7 +22,7 @@ class Message {
     }
 
     function set_messageid(){
-        if $this->message_id != NULL{
+        if ($this->message_id != NULL) {
             $this->message_id += 002010; // incrementing the user id to get more uniqure message ids (going to change it to a randomizer)
         } else {
             $this->message_id = 2000870000; //setting up the first messaage id (going to change it to message a randomizer)
@@ -30,7 +30,7 @@ class Message {
     }
 
     function set_message_text($message_text){
-        $this->message_text = $message_text
+        $this->message_text = $message_text;
     }
     function get_message_text(){
         return $this->message_text;
--- user.php
+++ user.php
@@ -1,15 +1,15 @@
 <?php
 class User{
     public $userName;
-    public $user_messages = []
-    public $user_id
+    public $user_messages = [];
+    public $user_id;
     // add more user definitions
 
     function __construct($userName){
         $this->userName = $userName;
         $this->message_time = time(); //to retrieve when a message was sent
         
-        set_userid();
+        $this->set_userid();
 
     }
 
@@ -18,11 +18,12 @@ class User{
         return $userName;
     }
 
-    function set_messageid(){
-        if $this->user_id != NULL{
+    function set_userid(){
+        if ($this->user_id != NULL) {
             $this->user_id += 002010; // incrementing the user id to get more uniqure user ids (going to change it to a randomizer)
         } else {
             $this->user_id = 2000870000; //setting up the first user id (going to change it to user a randomizer)
         }
     }
+} 
 ?>
